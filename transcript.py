a0a8d339108d6fc5e0294377095b200bb162e2ad
ryanovovo <ryanovovo@gmail.com>
2022-08-26 17:20:07 +0800

    平均成績四捨五入至小數點後第二位

--- transcript.py
+++ transcript.py
@@ -6,6 +6,7 @@ import os
 from functools import cmp_to_key
 
 
+# 計算該數字在哪個組內
 def interval(val):
     if val == 100:
         return 0
@@ -20,38 +21,51 @@ def interval(val):
     else:
         return 5
 
+#下載成績單模板
 def get_transcript_template():
     if not os.path.exists('template.xlsx'):
         wget.download('https://github.com/ryanovovo/Transcipt/raw/master/template.xlsx')
     
     return load_workbook('template.xlsx')
 
-
+# 自定義sort function
 def cmp_score(lhs, rhs):
     return rhs.weightedScoreSum - lhs.weightedScoreSum
 
+# 取得班級前十名的成績
 def get_top_ten_score(studentTranscripts):
     ret = []
     for i in range(10):
-        ret.append(studentTranscripts[i].weightedScoreAvg)
+        ret.append(round(studentTranscripts[i].weightedScoreAvg, 2))
     return ret
 
+# 取得組距表
 def get_intervals(studentTranscripts):
     ret = [[0 for x in range(7)] for y in range(6)] 
 
     for student in studentTranscripts:
         for i in range(0, 7):
-            ret[interval(student.studentScore[i])][i] += 1
+            ret[interval(student.score[i])][i] += 1
     return ret
 
+# 取得班級各科平均成績
+def get_class_avg_score(studentTranscripts):
+    ret = [0, 0, 0, 0, 0, 0, 0]
+    for student in studentTranscripts:
+        for i in range(0, 7):
+            ret[i] += student.score[i]
+    for i in range(0, 7):
+        ret[i] /= len(studentTranscripts)
+        ret[i] = round(ret[i], 2)
+    return ret
 
 
 class Student():
     def __init__(self, studentId, rawClassScore):
-        self.studentId = studentId
+        self.seatNumber = studentId 
         self.dataRow = 0
-        self.studentData = []
-        self.studentScore = []
+        self.rawData = []
+        self.score = []
         self.name = ''
         self.transcript = None
         self.weightedScoreSum = 0.0
@@ -59,78 +73,100 @@ class Student():
         self.rank = 0
         self.diff = 0
 
+        # 根據座號查表取得數據所在的row
         for i in range(1, 50):
-            if rawClassScore.cell(i, 1).value == self.studentId:
+            if rawClassScore.cell(i, 1).value == self.seatNumber:
                 self.dataRow = i
 
+        # 取得該row內的所有數據
         for cell in rawClassScore[str(self.dataRow)]:
-            self.studentData.append(cell.value)
+            self.rawData.append(cell.value)
 
-        self.name = self.studentData[1]
-        self.diff = self.studentData[12]
+        self.name = self.rawData[1]
+        self.diff = self.rawData[12]
         self.get_score()
         
+    #取得該學生的成績
     def get_score(self):
         weights = [5, 3 ,4, 3, 1, 1, 1]
         for i in range(2, 9):
-            self.studentScore.append(self.studentData[i])
+            self.score.append(self.rawData[i])
         
         self.weightedScoreSum = 0.0
         for i in range(7):
-            self.weightedScoreSum += weights[i] * self.studentScore[i]
+            self.weightedScoreSum += weights[i] * self.score[i]
         
         self.weightedScoreAvg = self.weightedScoreSum / 18
+        self.weightedScoreAvg = round(self.weightedScoreAvg, 2)
 
-        self.studentScore.append(self.weightedScoreSum)
-        self.studentScore.append(self.weightedScoreAvg)
+        self.score.append(self.weightedScoreSum)
+        self.score.append(self.weightedScoreAvg)
 
-    def generate_transcript(self, topTenScore, intervals):
+    # 生成成績單
+    def generate_transcript(self, topTenScore, intervals, classAvgScore):
         self.transcript = get_transcript_template()
         self.transcript.active['B3'] = self.name
-        self.transcript.active['A4'] = self.studentId
+        self.transcript.active['A4'] = self.seatNumber
         self.transcript.active['L4'] = self.rank
         self.transcript.active['M4'] = self.diff
+    
+        # 填入學生成績
         for i in range(9):
-            self.transcript.active.cell(4, i+3).value = self.studentScore[i]
+            self.transcript.active.cell(4, i+3).value = self.score[i]
         
+        # 填入組距表
         for i in range(6):
             for j in range(7):
                 self.transcript.active.cell(i+6, j+3).value = intervals[i][j]
 
+        # 填入前十名的加權平均成績
         for i in range(10):
             self.transcript.active.cell(10+i, 13).value = topTenScore[i]
-
+        
+        # 填入班級各科平均成績
+        for i in range(7):
+            self.transcript.active.cell(5, 3+i).value = classAvgScore[i]
+    # 將excel表存入資料夾
     def save_xlsx(self, path=''):
-        self.transcript.save(path+'/'+str(self.studentId)+str(self.name)+'.xlsx')
+        self.transcript.save(path+'/'+str(self.seatNumber)+str(self.name)+'.xlsx')
             
-
+# 讀取原始成績檔
 rawClassScore = load_workbook('score.xlsx')['Sheet2']
-studentIds = []
 
+#初始化資料
+seatNumbers = []
+studentTranscripts = []
+topTenScore = []
+
+# 取得所有學生的座號
 for cell in rawClassScore['a']:
     if type(cell.value) == int:
-        studentIds.append(cell.value)
+        seatNumbers.append(cell.value)
     
     elif type(cell.value) == None:
         break
 
-studentTranscripts = []
-topTenScore = []
+# 使用座號生成個人成績資訊
+for seatNumber in seatNumbers:
+    studentTranscripts.append(Student(seatNumber, rawClassScore))
 
-for studentId in studentIds:
-    studentTranscripts.append(Student(studentId, rawClassScore))
-
+# 使用加權總分排序成績
 studentTranscripts = sorted(studentTranscripts, key=cmp_to_key(cmp_score))
 
+# 填入排名
 for i in range(0, len(studentTranscripts)):
     studentTranscripts[i].rank = i+1
 
+# 取得前十名成績, 組聚, 班級各科平均成績
 topTenScore = get_top_ten_score(studentTranscripts)
 intervals = get_intervals(studentTranscripts)
+classAvgScore = get_class_avg_score(studentTranscripts)
 
+# 建立存放成績單的資料夾
 if not os.path.exists('transcripts'):
     os.mkdir('transcripts')
 
+# 生成成績單並存入資料夾
 for st in studentTranscripts:
-    st.generate_transcript(topTenScore, intervals)
+    st.generate_transcript(topTenScore, intervals, classAvgScore)
     st.save_xlsx('transcripts')
